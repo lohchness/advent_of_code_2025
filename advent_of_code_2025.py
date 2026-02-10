7baa4f816aacdc1db3bf14b579e8cedab6fb66ce
Lok/Tom Chow <lohchness@gmail.com>
2026-02-11 00:47:37 +1100

    day 1 part 2

--- advent_of_code_2025.py
+++ advent_of_code_2025.py
@@ -3,18 +3,32 @@ import sys
 
 def day_1(instructions: str):
     with open(instructions, "r") as f:
-        count = 0
         number = 50
         times_at_zero = 0
 
+        # for i in f.readlines():
+        #     direction = i[0]
+        #     length = int(''.join(i[1:]))
+
+        #     number += length if direction == "R" else -length
+        #     number = number % 100
+        #     if number == 0: times_at_zero += 1
+
+        # Naive part two - 6616
         for i in f.readlines():
             direction = i[0]
             length = int(''.join(i[1:]))
 
+            start = number
+            end = number + length if direction == "R" else number - length
+
+            for i in range(start, end, 1 if direction == "R" else -1):
+                if i % 100 == 0:
+                    times_at_zero += 1
+                    # print(start, direction+str(length),end)
+            
             number += length if direction == "R" else -length
             number = number % 100
-            if number == 0: times_at_zero += 1
-            count += 1
     
     return times_at_zero
 
